2aa2fda5ffded7d5f3a845def9dca07a91e5894a
Keith McKay <keith.mckay@scaleflux.com>
2022-06-27 14:30:21 -0700

    Remove column ordering requirement and separate parsing

--- runfio.py
+++ runfio.py
@@ -5,7 +5,6 @@ import os
 import argparse
 import csv
 import subprocess
-import json
 
 if __name__ == '__main__':
 
@@ -24,23 +23,24 @@ if __name__ == '__main__':
    parser.set_defaults(parse_only='store_false')
    (args) = parser.parse_args()
 
-   headers = []   # Will hold CSV header values 
-   fiojobs = []   # Will hold lists of FIO jobs to run 
+   headers  = []   # Will hold CSV header values 
+   fiojobs  = []   # Will hold lists of FIO jobs to create
+   fiofiles = []   # Will hold list if FIO files to run
 
    with open(args.csv, newline='') as csvfile:
-      csvdata_raw = csv.reader(csvfile, delimiter=',')
-
+      csvdata = csv.reader(csvfile, delimiter=',')
       # Remove comments and empty lines from the input CSV
-      for row in csvdata_raw:
+      for row in csvdata:
+         # Convert everything to lowercase and remove any leading/trailing spaces
+         row = [(x.lower()).strip() for x in row]
          if str(row[0]).startswith("#"):
             continue
-         elif str(row[0]).isspace():
+         if row == []:
             continue
          elif not headers:
-            headers = row   # Headers must come first 
+            headers = row 
          else:
             fiojobs.append(row)
-
       csvfile.close()
 
    # Create output path
@@ -51,36 +51,40 @@ if __name__ == '__main__':
    else:
       os.makedirs(path)
 
-   fiofiles = []   # Will hold set of FIO files to run
-
+   # Build the job files
    for job in fiojobs:
-
-      # The first column must be the filename, second column the testname, and third column whether to format
-      if job[0] in fiofiles:   
-         fiofile = open(os.path.join(args.out, str(job[0]).strip()), 'a')   # We are appending to an existing file
-         fiofile.write("\n" + "[" + str(job[1]).strip() + "]\n") 
-      else:
-         fiofile = open(os.path.join(args.out, str(job[0]).strip()), 'w+')   # We are writing a new file
-         if (str(job[2]).strip()).lower() == 'y' or (str(job[2]).strip()).lower() == 'yes':
-            fiofile.write("#FORMAT: Device will be formatted before the test(s) below\n")
-         fiofile.write("[" + str(job[1]).strip() + "]\n") 
-         fiofiles.append(job[0])
-
+      test = str(job[headers.index('file')])
+      nvmf = str(job[headers.index('format')])
+      name = str(job[headers.index('job')])
+      # Check if a format is needed before running the job file 
+      if nvmf.startswith("y") or nvmf.startswith("1") or nvmf.startswith("t"):
+         test = "format-" + test 
+      if test not in fiofiles and ("format-" + test) not in fiofiles:
+         fiofiles.append(test)
+      # Open FIO file to write
+      fiofile = open(os.path.join(args.out, test), 'a')
+      # Print the job name
+      fiofile.write("\n" + "[" + name + "]\n")
+      # Populate the drive to test
       fiofile.write("filename=" + str(args.ssd) + "\n") 
-
-      # Columns can be in any order after that...
-      for i in range(len(headers)):
-         if i < 3:   # We took care of these special cases above... 
+      # Fill in the rest of the values...
+      for i in range(len(job)):
+         # Special cases taken care of separately
+         if i == headers.index('file'):
             continue
+         elif i == headers.index('job'):
+            continue
+         elif i == headers.index('format'):
+            continue
+         # Everything else...
          elif str(job[i]) == '' or str(job[i]).isspace():
             continue
-         elif (str(job[i]).strip()).lower() == 'n' or (str(job[i]).strip()).lower() == 'no':
+         elif (str(job[i]).startswith("n") or str(job[i]).startswith("0") or str(job[i]).startswith("f")):
             continue
-         elif (str(job[i]).strip()).lower() == 'y' or (str(job[i]).strip()).lower() == 'yes':
-            fiofile.write(str(headers[i]).strip() + "\n")
+         elif (str(job[i]).startswith("y") or str(job[i]).startswith("1") or str(job[i]).startswith("t")):
+            fiofile.write(str(headers[i]) + "\n")
          else:
-            fiofile.write(str(headers[i]).strip() + "=" + str(job[i]).strip() + "\n")
-
+            fiofile.write(str(headers[i]) + "=" + str(job[i]) + "\n")
       fiofile.close()
 
    if args.parse_only is True:
@@ -89,47 +93,11 @@ if __name__ == '__main__':
    os.chdir(args.out)
 
    # Run the FIO jobs
-   for job in fiofiles:
-      with open(str(job).strip()) as fiofile:
-          if 'FORMAT' in fiofile.read():
-             subprocess.run(["nvme", "format", str(args.ssd), "--ses=1", "-f"])
-      fiofile.close()
-      subprocess.run(["fio", str(job).strip(), "--output-format=json+",
-         "--output=" + str(job).strip() + ".summary.log"]) 
-
-   # Put some basic results in a summary
-   summary = open("fio_summary.csv", "w+")
-
-   modes = ['read', 'write', 'trim']
-   stats = ['iops', 'iops_min', 'iops_max', 'iops_mean', 'iops_stddev',
-            'bw', 'bw_min', 'bw_max', 'bw_mean', 'bw_dev']
-
-   header = ['Test']
-   for mode in modes:
-      for stat in stats:
-         header.append(mode + "_" + stat)
-      header.append(mode + '_clat_ns_min')
-      header.append(mode + '_clat_ns_max')
-      header.append(mode + '_clat_ns_mean')
-      header.append(mode + '_clat_ns_stddev')
-   summary.write(','.join(header) + "\n")
-
-   for job in fiofiles:
-      with open(str(job).strip() + ".summary.log", "w") as summaryfile:
-         j = json.loads(summaryfile.read())
-         for job in j['jobs']:
-            values = []
-            values.append(job['jobname'])
-            for mode in modes:
-               for stat in stats:
-                  values.append(str(job[mode][stat]))
-               values.append(str(job[mode]['clat_ns']['min']))
-               values.append(str(job[mode]['clat_ns']['max']))
-               values.append(str(job[mode]['clat_ns']['mean']))
-               values.append(str(job[mode]['clat_ns']['stddev']))
-            summary.write(','.join(values) + "\n")         
-      summaryfile.close()
-   summary.close()
+   for fio in fiofiles:
+      if fio.startswith("format-"):   
+          subprocess.run(["nvme", "format", str(args.ssd), "--ses=1", "-f"])
+      subprocess.run(["fio", fio, "--output-format=json+",
+         "--output=" + fio + ".summary.log"]) 
 
 print("\nComplete\n")
 sys.exit()
